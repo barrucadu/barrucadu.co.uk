e179533608874243374ee453cf097952cb73f9d5
Michael Walker <mike@barrucadu.co.uk>
2011-09-25 16:01:16 +0100

    Touhou errors!

--- errors/error.php
+++ errors/error.php
@@ -7,48 +7,25 @@ $errors = array('404' => 'File Not Found',
 $error = $_GET['error'];
 $name  = $errors[$error];
 
-$characters = array('Konata Izumi', 'Kagami Hiiragi', 'Tsukasa Hiiragi');
+$errors = array (array ('character' => 'Patchouli Knowledge',
+                        'image'     => 'patchouli.jpg',
+                        'messages'  => array ('
+<strong>Patchouli:</strong> ...By the way, who are you?',
 
-$images = array('Konata Izumi'    => array('src' => 'konata.jpg',  'width' => '552', 'height' => '391'),
-                'Kagami Hiiragi'  => array('src' => 'kagami.jpg',  'width' => '400', 'height' => '250'),
-                'Tsukasa Hiiragi' => array('src' => 'tsukasa.jpg', 'width' => '370', 'height' => '396'));
+                                              '
+<strong>Patchouli:</strong> The books here are worth all the donations to your shrine for the past five years!<br/>
+<strong>Reimu:</strong> My shrine rarely has any visitors throughout the year.<br/>
+<strong>Patchouli:</strong> Oh. Then I guess it\'s worth nothing.',
 
-$quotes = array('Konata Izumi'    => array("The flatness is awesome!",
-                                           "So yeah, how's about that study group?",
-                                           "I don't know, but suddenly I'm thinkin' that a few of these cookies aren't as tasty as some of the other cookies are. Is this one of your cookies, Kagami?",
-                                           "Kay, I'm here, can I copy your homework now?",
-                                           "I've been lamenting my lack of a chest for a while but then this game said \"A flat chest is a status symbol! A rarity!\" Word for Word. And when you think about it there is a demand, right? Which means I''m valuable!",
-                                           "Bleh! Sounds like a lotta work to me. I think I'll pass, I'd rather play, and I'll ask for help when I get stuck!"),
+                                              '
+<strong>Patchouli:</strong> Don\'t take any books, please.')));
 
-                'Kagami Hiiragi'  => array("Can you stop guessing if they got the question right based on the amount of time left when they cut to commercial!",
-                                           "Oh yeah, I'm sure you'd know all about that! ...Wait a minute, you're in the eleventh grade, how do you know about erotic game senarios?!",
-                                           "What'd you say?!",
-                                           "Woah. That's a big butt. and a layered belly.",
-                                           "No! Like I said, Konata's not a normal girl. WE'RE the normal ones, lady!",
-                                           "Do your own homework!",
-                                           "Ah! Too intense for me!",
-                                           "Your just going to copy my answers aren't you?!",
-                                           "At least pretend to be guilty!",
-                                           "Mission failed",
-                                           "I have a name,you know",
-                                           "WOULD YOU CUT IT OUT ALREADY!?!"),
+shuffle($errors);
+shuffle($errors[0]['messages']);
 
-                'Tsukasa Hiiragi' => array("I'm not having fun; I'm really not having fun!",
-                                           "I don't have anything funny to say now...",
-                                           "Good morning...",
-                                           "Oh, homework!",
-                                           "Balsamic Vinegar!"));
-
-shuffle($characters);
-$character = $characters[0];
-shuffle($quotes[$character]);
-
-$src   = $images[$character]['src'];
-$w     = $images[$character]['width'];
-$h     = $images[$character]['height'];
-$alt   = "{$character} - Lucky Star";
-$cite  = $character;
-$quote = $quotes[$character][0];
+$src   = $errors[0]['image'];
+$alt   = "{$errors[0]['character']} - Touhou";
+$quote = $errors[0]['messages'][0];
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
@@ -67,9 +44,8 @@ $quote = $quotes[$character][0];
   <body>
     <h1><?php echo $error; ?></h1>
     <h2><?php echo $name; ?></h2>
-    <img src="/errors/<?php echo $src; ?>" alt="<?php echo $alt; ?>" width="<?php echo $w; ?>" height="<?php echo $h; ?>"/>
+    <img src="/errors/<?php echo $src; ?>" alt="<?php echo $alt; ?>"/>
     <div>
-      <span><?php echo $cite; ?></span>
       <blockquote><?php echo $quote; ?></blockquote>
     </div>
     <p><a href="http://www.barrucadu.co.uk" title="Return to Website">Return to Website</a></p>
