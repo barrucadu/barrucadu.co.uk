b8ee7af0c4850da27eec569bb7ff907a6606178d
Michael Walker <mike@barrucadu.co.uk>
2021-03-14 13:08:04 +0000

    Build with standard dockerhub images
    Since I've stopped using pandoc to render the markdown in this repo, a
    standard Python image does the job.

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.barrucadu.dev/barrucadu.co.uk-builder sh -c "
-  virtualenv venv
-  source venv/bin/activate
+docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src python:3.8 sh -c "
   pip install -r requirements.txt
   ./build --root=https://misc.barrucadu.co.uk/_site/ --out=/build"
